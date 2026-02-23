055b573bb35c89c251e6111ac4b108029449d98a
dhanisulistiyo <dhani.s.wibowo@gmail.com>
2026-02-23 20:29:50 +0700

    [feature] update di

--- internal/di/di.go
+++ internal/di/di.go
@@ -1,14 +1,18 @@
 package di
 
 import (
+	jobInfr "recon-service/internal/infrastructure/memory/job"
 	httpHandler "recon-service/internal/interface/http"
+	"recon-service/internal/usecase/job"
 	"recon-service/internal/usecase/reconcile"
 )
 
 func Initialize() *httpHandler.Handler {
-	// repo := &inMemoryRepo{}
-	service := reconcile.NewService()
-	handler := httpHandler.NewHandler(service)
+	jobRepo := jobInfr.NewInMemoryRepo()
+	reconSvc := reconcile.NewService()
+	jobSvc := job.NewService(jobRepo)
+	workerSvc := job.NewWorker(jobRepo, reconSvc)
+	handler := httpHandler.NewHandler(jobSvc, workerSvc)
 
 	return handler
 }
